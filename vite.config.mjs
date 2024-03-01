a94f644ce5bba89b798d0580b1433d1e19050b71
Ravi Babu Pathuri <ravibpathuri@gmail.com>
2024-03-01 12:02:31 +0530

    docker settings

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,14 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server:{
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 3000, // replace this port with any number you want
+  },
   test: {
     globals: true,
     environment: 'jsdom',
